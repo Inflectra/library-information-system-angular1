4d523adec4b856543505c50e1f27dac7e4f4cd53
Ali Pokrywka <alipokrywka@gmail.com>
2017-12-07 19:25:10 +0000

    Author functionality finished

--- js/controllers/authors.js
+++ js/controllers/authors.js
@@ -1,15 +1,13 @@
 /* global app */
 /* global angular */
 
-app.controller('authorsCtrl', ['$scope', 'authorService', function($scope, authorService) {
+app.controller('authorsCtrl', ['$scope', 'authorService', function($scope, authorService, $index) {
   
   $scope.include = function(obj) {
      authorService.updateAuthors(obj);
    };
    
   $scope.temp = authorService.authorsList;
-  
-  $scope.orig = angular.copy(authorService.authorsList);
   
   $scope.gridOptions = {
     
@@ -21,25 +19,33 @@ app.controller('authorsCtrl', ['$scope', 'authorService', function($scope, autho
     saveFocus: false,
     saveScroll: true,
     saveGroupingExpandedStates: true,
+    enableRowSelection: true,
+    enableRowHeaderSelection: false,
     onRegisterApi: function (gridApi) {
                    $scope.gridApi = gridApi;
                    },
   
     columnDefs: [
-    // { field: 'button', name: '', cellTemplate: '<button class="btn btn-warning ui-grid-cell-contents" ng-click="grid.appScope.editCell()"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>', width: 29, enableColumnMenu: false, enableCellEdit: false },
-       { field: 'id', displayName: 'ID', enableCellEdit: false, enableHiding: false,
+       //{ field: 'button', name: '', cellTemplate: '<button class="btn btn-warning ui-grid-cell-contents" ng-click="grid.appScope.deleteRow()"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>', width: 29, enableColumnMenu: false, enableCellEdit: false },
+       { field: 'id', displayName: 'ID', enableCellEdit: false, enableHiding: false
        },
-       { field: 'name', displayName: 'Name', enableHiding: false,
+       { field: 'name', displayName: 'Name', enableHiding: false
        },
-       { field: 'age', displayName: 'Age', enableHiding: false,
+       { field: 'age', displayName: 'Age', enableHiding: false
        }
     ]
   };
   
-  $scope.resetValues = function(){
-         $scope.temp = $scope.orig;
-
-  }
+  $scope.addNewItem = function() {
+    var n = $scope.temp.length + 1;
+    $scope.temp.push( { id: n, name: 'New Author', age: 0});
+    };
+    
+//   $scope.deleteSelected = function(row) {
+//     angular.forEach($scope.gridApi.selection.getSelectedRows(), function (data, index) {
+//     $scope.temp.splice($scope.temp.lastIndexOf(data), 1);
+//   });
+// };
   
 }]);
 

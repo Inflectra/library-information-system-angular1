868594da855c1d730b61094fa56ef17347f620d7
Ali <alipokrywka@gmail.com>
2017-12-14 19:02:24 -0800

    Updated files
     Login, routing, and author page completed with edit, save, and create new author functionality in place. Book page in progress with genre dropdown, save, delete, and create new book functionality completed. Author dropdown in progress.

---
+++ js/app.js
@@ -0,0 +1,49 @@
+/* global angular */
+/* global permissions */
+/*global $scope*/
+
+
+var app = angular.module('myApp', ['ui.router', 'ui.grid', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav', 'ui.grid.saveState']);
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+   $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+    .state("home", {
+        url : "/home",
+        templateUrl : "views/home.html",
+        controller : "homeCtrl"
+    })
+    .state("books", {
+        url : "/books",
+        templateUrl : "views/books.html",
+        controller : "booksCtrl"
+    })
+    .state("authors", {
+        url : "/authors",
+        templateUrl : "views/authors.html",
+        controller : "authorsCtrl"
+    })
+    .state("login", {
+        controller : "loginCtrl"
+    })
+    .state("nav", {
+        templateUrl : "views/nav.html",
+        controller : "navCtrl"
+    });
+})
+
+.run(['$rootScope', '$state', function($rootScope, $state) {
+ $rootScope.loggedIn = false;
+   $rootScope.$on('$stateChangeStart', function(e, to) {
+  
+       if ($rootScope.loggedIn == false && to.name != 'home') {
+       e.preventDefault();
+       alert('Please log in.')
+     }
+ 
+    });
+
+ }]);
+

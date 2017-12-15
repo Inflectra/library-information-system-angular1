777d796afb039814748c03d4082d50d66b966280
Ali Pokrywka <alipokrywka@gmail.com>
2017-12-15 02:39:26 +0000

    Working on author dropdown in books grid

--- js/controllers/bookService.js
+++ js/controllers/bookService.js
@@ -5,7 +5,7 @@ app.service('bookService', function ($http) {
   {
       id: 1, 
       name: "Hound of the Baskervilles", 
-      author: "Arthur Conan Doyle", 
+      author: 3, 
       genre: "Murder Mystery", 
       dateAdded: "1/18/2015", 
       outOfPrint: "No",
@@ -14,7 +14,7 @@ app.service('bookService', function ($http) {
   {
       id: 2, 
       name: "The Scowrers", 
-      author: "Arthur Conan Doyle", 
+      author: 3, 
       genre: "Murder Mystery", 
       dateAdded: "1/1/2016" , 
       outOfPrint: "Yes",
@@ -23,7 +23,7 @@ app.service('bookService', function ($http) {
   {
       id: 3, 
       name: "Amsterdam", 
-      author: "Ian McEwan", 
+      author: 1, 
       genre: "Contemporary Fiction", 
       dateAdded: "2/28/2015" , 
       outOfPrint: "No",
@@ -32,7 +32,7 @@ app.service('bookService', function ($http) {
   {
       id: 4, 
       name: "Saturday", 
-      author: "Ian McEwan", 
+      author: 1, 
       genre: "Contemporary Fiction", 
       dateAdded: "2/9/2015" , 
       outOfPrint: "No",
@@ -41,7 +41,7 @@ app.service('bookService', function ($http) {
   {
       id: 5, 
       name: "The Comfort of Strangers", 
-      author: "Ian McEwan", 
+      author: 1, 
       genre: "Contemporary Fiction", 
       dateAdded: "4/15/2016" , 
       outOfPrint: "No",
@@ -50,7 +50,7 @@ app.service('bookService', function ($http) {
   {
       id: 6, 
       name: "Chesil Beach", 
-      author: "Ian McEwan", 
+      author: 1, 
       genre: "Contemporary Fiction", 
       dateAdded: "6/22/2015" , 
       outOfPrint: "No",
@@ -59,7 +59,7 @@ app.service('bookService', function ($http) {
   {
       id: 7, 
       name: "Atonement", 
-      author: "Ian McEwan", 
+      author: 1, 
       genre: "Historical Fiction", 
       dateAdded: "1/17/2016" , 
       outOfPrint: "No",
@@ -68,7 +68,7 @@ app.service('bookService', function ($http) {
   {
       id: 8, 
       name: "Bleak House", 
-      author: "Charles Dickens", 
+      author: 2, 
       genre: "Historical Fiction", 
       dateAdded: "12/13/2015" , 
       outOfPrint: "No",
@@ -77,7 +77,7 @@ app.service('bookService', function ($http) {
   {
       id: 9, 
       name: "Oliver Twist", 
-      author: "Charles Dickens", 
+      author: 2, 
       genre: "Historical Fiction", 
       dateAdded: "4/11/2016" , 
       outOfPrint: "No",
@@ -86,7 +86,7 @@ app.service('bookService', function ($http) {
   {
       id: 10, 
       name: "Nicholas Nickleby", 
-      author: "Charles Dickens", 
+      author: 2, 
       genre: "Historical Fiction", 
       dateAdded: "5/17/2016" , 
       outOfPrint: "No",
@@ -95,7 +95,7 @@ app.service('bookService', function ($http) {
   {
       id: 11, 
       name: "David Copperfield", 
-      author: "Charles Dickens", 
+      author: 2, 
       genre: "Historical Fiction", 
       dateAdded: "1/9/2015" , 
       outOfPrint: "No",
@@ -104,7 +104,7 @@ app.service('bookService', function ($http) {
   {
       id: 12, 
       name: "The Pickwick Papers", 
-      author: "Charles Dickens", 
+      author: 2, 
       genre: "Historical Fiction", 
       dateAdded: "8/1/2015" , 
       outOfPrint: "No",
@@ -113,7 +113,7 @@ app.service('bookService', function ($http) {
   {
       id: 13, 
       name: "Death on the Nile", 
-      author:  "Agatha Christie", 
+      author: 4, 
       genre: "Murder Mystery", 
       dateAdded: "6/2/2016" , 
       outOfPrint: "No",
@@ -122,7 +122,7 @@ app.service('bookService', function ($http) {
   {
       id: 14, 
       name: "Betrams Hotel", 
-      author:  "Agatha Christie", 
+      author: 4, 
       genre: "Murder Mystery", 
       dateAdded: "5/11/2015" , 
       outOfPrint: "No",
--- js/controllers/books.js
+++ js/controllers/books.js
@@ -1,138 +1,7 @@
 /* global app */
 /* global angular */
-// app.controller('booksCtrl', function($scope) {
 
-
-// $scope.booksList = [
-//   {
-//       id: 1, 
-//       name: "Hound of the Baskervilles", 
-//       author: "Arthur Conan Doyle", 
-//       genre: "Murder Mystery", 
-//       dateAdded: "1/18/2015", 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 2, 
-//       name: "The Scowrers", 
-//       author: "Arthur Conan Doyle", 
-//       genre: "Murder Mystery", 
-//       dateAdded: "1/1/2016" , 
-//       outOfPrint: "Yes",
-//       active: true
-//   },
-//   {
-//       id: 3, 
-//       name: "Amsterdam", 
-//       author: "Ian McEwan", 
-//       genre: "Contemporary Fiction", 
-//       dateAdded: "2/28/2015" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 4, 
-//       name: "Saturday", 
-//       author: "Ian McEwan", 
-//       genre: "Contemporary Fiction", 
-//       dateAdded: "2/9/2015" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 5, 
-//       name: "The Comfort of Strangers", 
-//       author: "Ian McEwan", 
-//       genre: "Contemporary Fiction", 
-//       dateAdded: "4/15/2016" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 6, 
-//       name: "Chesil Beach", 
-//       author: "Ian McEwan", 
-//       genre: "Contemporary Fiction", 
-//       dateAdded: "6/22/2015" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 7, 
-//       name: "Atonement", 
-//       author: "Ian McEwan", 
-//       genre: "Historical Fiction", 
-//       dateAdded: "1/17/2016" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 8, 
-//       name: "Bleak House", 
-//       author: "Charles Dickens", 
-//       genre: "Historical Fiction", 
-//       dateAdded: "12/13/2015" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 9, 
-//       name: "Oliver Twist", 
-//       author: "Charles Dickens", 
-//       genre: "Historical Fiction", 
-//       dateAdded: "4/11/2016" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 10, 
-//       name: "Nicholas Nickleby", 
-//       author: "Charles Dickens", 
-//       genre: "Historical Fiction", 
-//       dateAdded: "5/17/2016" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 11, 
-//       name: "David Copperfield", 
-//       author: "Charles Dickens", 
-//       genre: "Historical Fiction", 
-//       dateAdded: "1/9/2015" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 12, 
-//       name: "The Pickwick Papers", 
-//       author: "Charles Dickens", 
-//       genre: "Historical Fiction", 
-//       dateAdded: "8/1/2015" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 13, 
-//       name: "Death on the Nile", 
-//       author:  "Agatha Christie", 
-//       genre: "Murder Mystery", 
-//       dateAdded: "6/2/2016" , 
-//       outOfPrint: "No",
-//       active: true
-//   },
-//   {
-//       id: 14, 
-//       name: "Betrams Hotel", 
-//       author:  "Agatha Christie", 
-//       genre: "Murder Mystery", 
-//       dateAdded: "5/11/2015" , 
-//       outOfPrint: "No",
-//       active: true
-//   }
-// ];
-
-app.controller('booksCtrl', ['$scope', 'bookService', function($scope, bookService, $index) {
+app.controller('booksCtrl', ['$scope', 'bookService', 'authorService', function($scope, bookService, authorService, $index) {
   
   $scope.include = function(obj) {
      bookService.updateBooks(obj);
@@ -140,9 +9,10 @@ app.controller('booksCtrl', ['$scope', 'bookService', function($scope, bookServi
    
   $scope.bookTemp = bookService.booksList;
   
-  $scope.genreTypes = ['Murder Mystery',
- 'Historical Fiction',
- 'Contemporary Fiction' 
+  $scope.genreTypes = [
+    {id:"Murder Mystery", value:"Murder Mystery"},
+    {id:"Historical Fiction", value:"Historical Fiction"},
+    {id:"Contemporary Fiction", value:"Contemporary Fiction"} 
   ];
   
   $scope.bookGridOptions = {
@@ -167,10 +37,12 @@ app.controller('booksCtrl', ['$scope', 'bookService', function($scope, bookServi
        { field: 'name', displayName: 'Name', enableHiding: false
        },
        { field: 'author', displayName: 'Author', enableHiding: false,
-         editType: 'dropdown', editableCellTemplate: 'tempAuthor.html'
+         editableCellTemplate: 'ui-grid/dropdownEditor',
+         editDropdownRowEntityOptionsArrayPath: $scope.authorNames
        },
        { field: 'genre', name: 'genre', displayName: 'Genre', enableHiding: false,
-         editType: 'dropdown', editableCellTemplate: 'temp.html'
+         editableCellTemplate: 'ui-grid/dropdownEditor',
+         editDropdownOptionsArray: $scope.genreTypes
        },
        { field: 'dateAdded', displayName: 'Date Added', enableCellEdit: false, enableHiding: false
        },
@@ -181,10 +53,15 @@ app.controller('booksCtrl', ['$scope', 'bookService', function($scope, bookServi
     ]
   };
   
+//   $scope.booksIds = [];
+//       for(var i = 0; i < $scope.bookTemp.length; i++){
+//           $scope.booksIds.push($scope.bookTemp[i].author);
+
+//   };
+  
 var today = new Date();
 var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 
-  
    $scope.addNewBook = function() {
      var n = $scope.bookTemp.length + 1;
      $scope.bookTemp.push( { id: n, name: 'New Book', author: "", genre: "", dateAdded: date, outOfPrint: "No"  });
@@ -198,3 +75,5 @@ var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 
 
 }]);
+
+//http://ui-grid.info/docs/#/api/ui.grid.edit.api:ColumnDef
